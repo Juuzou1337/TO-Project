e2c025398db7968c80cb87ba637da23b38956415
Juuzou1337 <cucx@mail.ru>
2018-04-16 23:18:34 +0300

    Create ToolBar view

--- TO/app/src/main/java/com/example/user/to/MainActivity.java
+++ TO/app/src/main/java/com/example/user/to/MainActivity.java
@@ -3,11 +3,31 @@ package com.example.user.to;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.MenuItem;
+import android.widget.Toolbar;
 
 public class MainActivity extends Activity{
+
+    private android.support.v7.widget.Toolbar toolbar;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initToolBar();
+    }
+
+    private void initToolBar() {
+        toolbar = (android.support.v7.widget.Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.app_name);
+        toolbar.setOnMenuItemClickListener(new android.support.v7.widget.Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                return false;
+            }
+        });
+
+        toolbar.inflateMenu(R.menu.menu);
     }
 }
